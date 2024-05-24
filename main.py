75abeca629dd3245a93cdb52c7ce42fd094ba7b5
logan-tolbert <logo88205+git@gmail.com>
2024-05-24 18:58:39 -0500

    Complete project, add txt file

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python3
 from sys import argv
+
+
 
 script, f_path = argv
 
 def main():
     text = get_content(f_path)
     word_count = count_words(text)
-    # Prints content of file to terminal
-    # print(text)
-    print(f"Word count: {word_count}")
+    charDict = count_chars(text)
+    sortedCharlst = dict_to_sortd_lst(charDict)
+
+    print(f"--- Begin report of {f_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in sortedCharlst:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_content(path):
@@ -19,6 +32,28 @@ def get_content(path):
 def count_words(text):
     words = text.split()
     return(len(words))
+
+
+def count_chars(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_on(dictionary):
+    return dictionary["num"]
+
+
+def dict_to_sortd_lst(charDict):
+    sorted_list = []
+    for ch in charDict:
+        sorted_list.append({"char": ch, "num": charDict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
 
