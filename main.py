239e3ed6c8ab29447a7ba3e5cc55b28a65429e0d
logan-tolbert <lodev205@gmail.com>
2024-11-10 17:21:10 -0600

    refactor main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def main():
         contents = f.read()
         generate_report(contents)
         
+        
 def count_words(text):
     return len(text.split())
 
-def count_char_occurrence(text):
+
+def count_alphabetic_chars(text):
     char_count = {}
     for char in text.lower():
-
         if not char.isalpha():
             continue
         if char not in char_count:
@@ -18,6 +19,7 @@ def count_char_occurrence(text):
             char_count[char] += 1
     return char_count
 
+
 def generate_report(text):
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{count_words(text)} words found in the document")
@@ -25,6 +27,7 @@ def generate_report(text):
     for char, count in count_char_occurrence(text).items():
         print(f"The '{char}' was found {count} times")
     print("--- End report ---")
+    
     
 if __name__ == "__main__":  
     main()
