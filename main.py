0b886fffd475336ca267da316259c1a4bfeb1e9c
logan-tolbert <lodev205@gmail.com>
2024-11-10 17:05:50 -0600

    update main.py - Count words

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
-        print(contents)
+        print(count_words(contents))
+        
 
+def count_words(text):
+    return len(text.split())
 
 if __name__ == "__main__":  
     main()
