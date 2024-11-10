525fbfe06a815bd1aa54c2afab6649178760504b
logan-tolbert <lodev205@gmail.com>
2024-11-10 17:19:38 -0600

    update main.py - Handle non-alphabetic characters

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ def main():
 def count_words(text):
     return len(text.split())
 
-def count_char_occurence(text):
+def count_char_occurrence(text):
     char_count = {}
     for char in text.lower():
+
+        if not char.isalpha():
+            continue
         if char not in char_count:
             char_count[char] = 1
         else:
@@ -19,7 +22,7 @@ def generate_report(text):
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{count_words(text)} words found in the document")
     print()
-    for char, count in count_char_occurence(text).items():
+    for char, count in count_char_occurrence(text).items():
         print(f"The '{char}' was found {count} times")
     print("--- End report ---")
     
